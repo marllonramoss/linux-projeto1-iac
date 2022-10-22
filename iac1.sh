d10307cb2ba90a0980d0009fa498a89fe3cfc961
marllonramoos <marllonsteam@gmail.com>
2022-10-22 17:53:55 +0000

    Arquivos IaC v1

--- iac1.sh
+++ iac1.sh
@@ -20,23 +20,24 @@ useradd maria -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
 useradd joao -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
 
 useradd debora -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd sebastiano -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
 useradd roberto -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
 
 useradd josefina -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
 
-echo "Especificando permissoes dos diretorios..."
+echo "Especificando permissoes aos diretorios..."
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
 
-chmod 770 /adm
+chmod 777 /adm
 chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico
 
-echo "---FIM---"
+echo "---FIM!---"
+
 
